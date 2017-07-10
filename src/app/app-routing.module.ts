5dfa0b2f61b5f9313fbe8254efa87569cefb01d8
Jack Lam <jacksylam@gmail.com>
2017-07-09 16:25:25 -1000

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {RechargeComponent} from './recharge/recharge.component'
 
 const routes: Routes = [
   {
     path: '',
     children: []
-  }
+  },
+    { path: 'recharge',  component: RechargeComponent },
+
 ];
 
 @NgModule({
